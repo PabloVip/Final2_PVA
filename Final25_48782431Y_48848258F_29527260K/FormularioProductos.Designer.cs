560da7cb8ce26f6f6cc5c2b0d8a539c66ec67509
pablopardoo12 <ppardo8@alu.ucam.edu>
2025-05-11 20:49:01 +0200

    carrito

--- Final25_48782431Y_48848258F_29527260K/FormularioProductos.Designer.cs
+++ Final25_48782431Y_48848258F_29527260K/FormularioProductos.Designer.cs
@@ -28,22 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridproductos = new System.Windows.Forms.DataGridView();
             this.label_productos = new System.Windows.Forms.Label();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.dataGridcarrito = new System.Windows.Forms.DataGridView();
+            this.label_carrito = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridproductos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridcarrito)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dataGridproductos
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(92, 138);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(434, 663);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridproductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridproductos.Location = new System.Drawing.Point(92, 138);
+            this.dataGridproductos.Name = "dataGridproductos";
+            this.dataGridproductos.RowHeadersWidth = 62;
+            this.dataGridproductos.RowTemplate.Height = 28;
+            this.dataGridproductos.Size = new System.Drawing.Size(434, 663);
+            this.dataGridproductos.TabIndex = 0;
             // 
             // label_productos
             // 
@@ -54,28 +55,38 @@
             this.label_productos.TabIndex = 1;
             this.label_productos.Text = "Productos";
             // 
-            // dataGridView2
+            // dataGridcarrito
             // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(632, 138);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.RowHeadersWidth = 62;
-            this.dataGridView2.RowTemplate.Height = 28;
-            this.dataGridView2.Size = new System.Drawing.Size(483, 663);
-            this.dataGridView2.TabIndex = 2;
+            this.dataGridcarrito.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridcarrito.Location = new System.Drawing.Point(632, 138);
+            this.dataGridcarrito.Name = "dataGridcarrito";
+            this.dataGridcarrito.RowHeadersWidth = 62;
+            this.dataGridcarrito.RowTemplate.Height = 28;
+            this.dataGridcarrito.Size = new System.Drawing.Size(483, 663);
+            this.dataGridcarrito.TabIndex = 2;
+            // 
+            // label_carrito
+            // 
+            this.label_carrito.AutoSize = true;
+            this.label_carrito.Location = new System.Drawing.Point(633, 106);
+            this.label_carrito.Name = "label_carrito";
+            this.label_carrito.Size = new System.Drawing.Size(84, 30);
+            this.label_carrito.TabIndex = 3;
+            this.label_carrito.Text = "Carrito";
             // 
             // FormularioProductos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1201, 899);
-            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label_carrito);
+            this.Controls.Add(this.dataGridcarrito);
             this.Controls.Add(this.label_productos);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridproductos);
             this.Name = "FormularioProductos";
             this.Text = "FormularioProductos";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridproductos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridcarrito)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -83,8 +94,9 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridproductos;
         private System.Windows.Forms.Label label_productos;
-        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridcarrito;
+        private System.Windows.Forms.Label label_carrito;
     }
 }
