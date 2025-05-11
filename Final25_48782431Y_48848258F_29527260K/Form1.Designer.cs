f76ccca522fd2c959f2e459f95f53f7c986b7a4c
PabloVip <pablovicente29@hotmail.com>
2025-05-11 17:29:06 +0200

    login v1

--- Final25_48782431Y_48848258F_29527260K/Form1.Designer.cs
+++ Final25_48782431Y_48848258F_29527260K/Form1.Designer.cs
@@ -28,14 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_login = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_login.Location = new System.Drawing.Point(148, 377);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(75, 23);
+            this.btn_login.TabIndex = 0;
+            this.btn_login.Text = "LOGIN";
+            this.btn_login.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(772, 1065);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.ClientSize = new System.Drawing.Size(386, 552);
+            this.Controls.Add(this.btn_login);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -43,6 +54,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_login;
     }
 }
 
