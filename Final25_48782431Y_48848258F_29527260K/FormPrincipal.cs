9c9b2894654722fa63aa9cfc995f11053f05cbc9
pablopardoo12 <ppardo8@alu.ucam.edu>
2025-05-11 17:02:24 +0200

    crea formulario

---
+++ Final25_48782431Y_48848258F_29527260K/FormPrincipal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final25_48782431Y_48848258F_29527260K
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
