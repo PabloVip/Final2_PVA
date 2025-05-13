67abbda1acd5045f5cd85b9e522a1a0d28603f31
pablopardoo12 <ppardo8@alu.ucam.edu>
2025-05-13 16:03:30 +0200

    factura

--- Final25_48782431Y_48848258F_29527260K/FormularioFactura.Designer.cs
+++ Final25_48782431Y_48848258F_29527260K/FormularioFactura.Designer.cs
@@ -28,19 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.richTextBoxFactura = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // richTextBoxFactura
+            // 
+            this.richTextBoxFactura.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxFactura.Location = new System.Drawing.Point(0, 0);
+            this.richTextBoxFactura.Name = "richTextBoxFactura";
+            this.richTextBoxFactura.ReadOnly = true;
+            this.richTextBoxFactura.Size = new System.Drawing.Size(958, 473);
+            this.richTextBoxFactura.TabIndex = 0;
+            this.richTextBoxFactura.Text = "";
             // 
             // FormularioFactura
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(958, 473);
+            this.Controls.Add(this.richTextBoxFactura);
             this.Name = "FormularioFactura";
             this.Text = "FormularioFactura";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxFactura;
     }
 }
