878c73997e345bab73a34a1bbe161bb326964529
Ireneyss <irenedeyecla@gmail.com>
2025-05-14 11:54:19 +0200

    form closing form admin arreglado

--- Final25_48782431Y_48848258F_29527260K/FormAdmin.cs
+++ Final25_48782431Y_48848258F_29527260K/FormAdmin.cs
@@ -17,6 +17,7 @@ namespace Final25_48782431Y_48848258F_29527260K
         public FormAdmin(String usuario)
         {
             InitializeComponent();
+            this.FormClosing += FormAdmin_FormClosing;
         }
 
         private void btncerrarsesion_Click(object sender, EventArgs e)
@@ -26,6 +27,27 @@ namespace Final25_48782431Y_48848258F_29527260K
             this.Close();
             Form1 nuevoFormulario = new Form1();
             nuevoFormulario.Show();
+        }
+
+        private void FormAdmin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Solo mostramos el mensaje de confirmación si no estamos cerrando la aplicación a través de un proceso explícito
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                var confirmResult = MessageBox.Show("¿Estás seguro de que quieres salir?",
+                                                     "Confirmar salida",
+                                                     MessageBoxButtons.YesNo,
+                                                     MessageBoxIcon.Question);
+
+                if (confirmResult == DialogResult.Yes)
+                {
+                    Application.Exit();
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
         }
 
         private void FormAdmin_Load(object sender, EventArgs e)
