d94798ec9b18055b00dfc6e532e8efa5afa77aa5
pablopardoo12 <ppardo8@alu.ucam.edu>
2025-05-18 17:11:03 +0200

    e

--- Final25_48782431Y_48848258F_29527260K/FormularioKits.Designer.cs
+++ Final25_48782431Y_48848258F_29527260K/FormularioKits.Designer.cs
@@ -142,7 +142,7 @@
             this.listView1.FullRowSelect = true;
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(10, 202);
-            this.listView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listView1.Margin = new System.Windows.Forms.Padding(2);
             this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(1550, 693);
